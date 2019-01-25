823aa5c9def630074913293ee0d0fddbcdc40ee3
Linus <linus.lagerhjelm@gmail.com>
2019-01-25 09:38:13 +0100

    Create sunny day test case

--- test_aes.py
+++ test_aes.py
@@ -1,4 +1,12 @@
 from unittest import TestCase
 import AES
 
+input = b'linuslagerhjelm1'
+key = b'secretkeybytes19'
+
+
 class TestAES(TestCase):
+    def test_sunny_day_encryption(self):
+        expected = 'd17Mm4oZ1yfuIMPds6m0eDLYTQgauSnIy0t/ENKay7A='
+        actual = AES.encrypt(input, key)
+        self.assertEqual(expected, actual)
